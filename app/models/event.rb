ad37a738fa3cdb41783cfad1455b2ab9cc3fd7f7
gusvicuna <gpvicuna@miuandes.cl>
2019-04-03 11:50:41 -0300

    Baba

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,20 @@
+class AddUniqueIndexToEvents < ActiveRecord::Migration
+  def change
+    add_index :events, [:start_date, :event_venue], unique: true
+  end
+end
+
+
+
 class Event < ApplicationRecord
   belongs_to :event_venue
   has_many :ticket_types
+  validates :start_date, presence:true
+  validate :start_date_cannot_be_in_the_past
+
+  def start_date_cannot_be_in_the_past
+    if start_date.present? && start_date < Date.today
+      errors.add(:start_date, "can't be in the past")
+    end
+  end
 end
