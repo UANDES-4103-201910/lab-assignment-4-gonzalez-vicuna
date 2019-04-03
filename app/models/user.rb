cf3793cad1aadf04c2878d7967245fa88f77dcaf
barracius <rjgonzalez@miuandes.cl>
2019-04-03 10:10:43 -0300

    Fixed alphanumerical value on User.password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  validates_format_of :password, :with => /\A[A-Za-z0-9]+\z/
   validates :password, length: {in: 8..12}
   validates :phone_number, length: {in:9..12}
   validates_format_of :email, :with => /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/
