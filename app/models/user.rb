e7d74eb31f3676c15050f17fe10aab0b6abdbd89
barracius <rjgonzalez@miuandes.cl>
2019-04-03 10:00:11 -0300

    Users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ApplicationRecord
+  validates :password, length: {in: 8..12}
+  validates :phone_number, length: {in:9..12}
+  validates_format_of :email, :with => /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/
   has_many :orders
 end
