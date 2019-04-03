6ba8419208fc5f586982b644f298965b200a33ab
gusvicuna <gpvicuna@miuandes.cl>
2019-04-03 10:47:52 -0300

    Ticket done

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,4 +1,14 @@
 class Ticket < ApplicationRecord
+  validate :event_date_cannot_be_in_the_past
+
+
+  def event_date_cannot_be_in_the_past
+    eventdate=self.ticket_type.event.start_date
+    if eventdate.present? && eventdate < Date.today
+      errors.add("Event can't be in the past")
+    end
+  end
+
   belongs_to :ticket_type
   belongs_to :order
 end
