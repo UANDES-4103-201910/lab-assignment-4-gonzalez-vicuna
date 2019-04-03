311f4b4c585bb3bc47694d4d6b9e970eaace4645
gusvicuna <gpvicuna@miuandes.cl>
2019-04-03 12:34:48 -0300

    Done

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,6 +3,7 @@ class Event < ApplicationRecord
   has_many :ticket_types
   validates :start_date, presence:true
   validate :start_date_cannot_be_in_the_past
+  validates :start_date, uniqueness: { scope: :event_venue}
 
   def start_date_cannot_be_in_the_past
     if start_date.present? && start_date < Date.today
